38d48e725c188b37b78b9e0206d3dcf6ced0d08a
Luc Sieben <lucsieben97@hotmail.com>
2016-05-11 16:10:27 +0200

    Added system class and card class.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,23 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(62, 12);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(121, 97);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Controls.Add(this.listView1);
+            this.ClientSize = new System.Drawing.Size(212, 206);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,7 +44,6 @@
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,13 @@ namespace poker
 {
     public partial class Form1 : Form
     {
+        public system sys = new system();
+
         public Form1()
         {
             InitializeComponent();
+            // load cards
+            sys.loadcards();
         }
     }
 }
