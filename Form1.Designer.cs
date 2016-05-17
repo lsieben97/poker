bb2bbdf55e6d9068f106f8c4feb23aa539910444
jennebumke <jennebumke@live.nl>
2016-05-17 10:38:17 +0200

    Form updates
    Added icon
    Created default settings

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace poker
 {
-    partial class Form1
+    partial class frmPokerLJ
     {
         /// <summary>
         /// Required designer variable.
@@ -28,18 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPokerLJ));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.msMenuLJ = new System.Windows.Forms.MenuStrip();
+            this.tsmiGameLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiExtraLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiRefreshLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiRestartLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiExitLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAboutLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiHelpLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiDevelopersLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiDevLucLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiDevJenneLJ = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAboutGameLJ = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.msMenuLJ.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -90,7 +104,108 @@
             this.pictureBox6.TabIndex = 0;
             this.pictureBox6.TabStop = false;
             // 
-            // Form1
+            // msMenuLJ
+            // 
+            this.msMenuLJ.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.msMenuLJ.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiGameLJ,
+            this.tsmiExtraLJ});
+            this.msMenuLJ.Location = new System.Drawing.Point(0, 0);
+            this.msMenuLJ.Name = "msMenuLJ";
+            this.msMenuLJ.Size = new System.Drawing.Size(964, 28);
+            this.msMenuLJ.TabIndex = 1;
+            this.msMenuLJ.Text = "menuStrip1";
+            // 
+            // tsmiGameLJ
+            // 
+            this.tsmiGameLJ.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiRefreshLJ,
+            this.tsmiRestartLJ,
+            this.tsmiExitLJ});
+            this.tsmiGameLJ.Name = "tsmiGameLJ";
+            this.tsmiGameLJ.Size = new System.Drawing.Size(60, 24);
+            this.tsmiGameLJ.Text = "Game";
+            // 
+            // tsmiExtraLJ
+            // 
+            this.tsmiExtraLJ.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiAboutLJ,
+            this.tsmiHelpLJ});
+            this.tsmiExtraLJ.Name = "tsmiExtraLJ";
+            this.tsmiExtraLJ.Size = new System.Drawing.Size(54, 24);
+            this.tsmiExtraLJ.Text = "Extra";
+            // 
+            // tsmiRefreshLJ
+            // 
+            this.tsmiRefreshLJ.Name = "tsmiRefreshLJ";
+            this.tsmiRefreshLJ.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.tsmiRefreshLJ.Size = new System.Drawing.Size(187, 26);
+            this.tsmiRefreshLJ.Text = "Refresh";
+            this.tsmiRefreshLJ.Click += new System.EventHandler(this.tsmiRefreshLJ_Click);
+            // 
+            // tsmiRestartLJ
+            // 
+            this.tsmiRestartLJ.Name = "tsmiRestartLJ";
+            this.tsmiRestartLJ.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F5)));
+            this.tsmiRestartLJ.Size = new System.Drawing.Size(187, 26);
+            this.tsmiRestartLJ.Text = "Restart";
+            this.tsmiRestartLJ.Click += new System.EventHandler(this.tsmiRestartLJ_Click);
+            // 
+            // tsmiExitLJ
+            // 
+            this.tsmiExitLJ.Name = "tsmiExitLJ";
+            this.tsmiExitLJ.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.tsmiExitLJ.Size = new System.Drawing.Size(187, 26);
+            this.tsmiExitLJ.Text = "Exit";
+            this.tsmiExitLJ.Click += new System.EventHandler(this.tsmiExitLJ_Click);
+            // 
+            // tsmiAboutLJ
+            // 
+            this.tsmiAboutLJ.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiDevelopersLJ,
+            this.tsmiAboutGameLJ});
+            this.tsmiAboutLJ.Name = "tsmiAboutLJ";
+            this.tsmiAboutLJ.Size = new System.Drawing.Size(181, 26);
+            this.tsmiAboutLJ.Text = "About";
+            // 
+            // tsmiHelpLJ
+            // 
+            this.tsmiHelpLJ.Name = "tsmiHelpLJ";
+            this.tsmiHelpLJ.Size = new System.Drawing.Size(181, 26);
+            this.tsmiHelpLJ.Text = "Help";
+            this.tsmiHelpLJ.Click += new System.EventHandler(this.tsmiHelpLJ_Click);
+            // 
+            // tsmiDevelopersLJ
+            // 
+            this.tsmiDevelopersLJ.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiDevLucLJ,
+            this.tsmiDevJenneLJ});
+            this.tsmiDevelopersLJ.Name = "tsmiDevelopersLJ";
+            this.tsmiDevelopersLJ.Size = new System.Drawing.Size(181, 26);
+            this.tsmiDevelopersLJ.Text = "Developers";
+            // 
+            // tsmiDevLucLJ
+            // 
+            this.tsmiDevLucLJ.Name = "tsmiDevLucLJ";
+            this.tsmiDevLucLJ.Size = new System.Drawing.Size(221, 26);
+            this.tsmiDevLucLJ.Text = "Luc Sieben";
+            this.tsmiDevLucLJ.Click += new System.EventHandler(this.tsmiDevLucLJ_Click);
+            // 
+            // tsmiDevJenneLJ
+            // 
+            this.tsmiDevJenneLJ.Name = "tsmiDevJenneLJ";
+            this.tsmiDevJenneLJ.Size = new System.Drawing.Size(221, 26);
+            this.tsmiDevJenneLJ.Text = "Jenne van den Boom";
+            this.tsmiDevJenneLJ.Click += new System.EventHandler(this.tsmiDevJenneLJ_Click);
+            // 
+            // tsmiAboutGameLJ
+            // 
+            this.tsmiAboutGameLJ.Name = "tsmiAboutGameLJ";
+            this.tsmiAboutGameLJ.Size = new System.Drawing.Size(181, 26);
+            this.tsmiAboutGameLJ.Text = "Game";
+            this.tsmiAboutGameLJ.Click += new System.EventHandler(this.tsmiAboutGameLJ_Click);
+            // 
+            // frmPokerLJ
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -101,15 +216,21 @@
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.msMenuLJ);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.msMenuLJ;
+            this.Name = "frmPokerLJ";
+            this.Text = "Digital Poker";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.msMenuLJ.ResumeLayout(false);
+            this.msMenuLJ.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -121,6 +242,18 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.MenuStrip msMenuLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiGameLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiRefreshLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiRestartLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExitLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExtraLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAboutLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDevelopersLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDevLucLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDevJenneLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiHelpLJ;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAboutGameLJ;
 
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,53 @@ using System.Windows.Forms;
 
 namespace poker
 {
-    public partial class Form1 : Form
+    public partial class frmPokerLJ : Form
     {
-        public Form1()
+        public frmPokerLJ()
         {
             InitializeComponent();
+        }
+
+        private void tsmiExitLJ_Click(object sender, EventArgs e)
+        {
+            //close application
+            Application.Exit();
+        }
+
+        private void tsmiRestartLJ_Click(object sender, EventArgs e)
+        {
+            //restart application
+            Application.Restart();
+        }
+
+        private void tsmiRefreshLJ_Click(object sender, EventArgs e)
+        {
+            //refresh application
+            this.Refresh();
+        }
+
+        private void tsmiHelpLJ_Click(object sender, EventArgs e)
+        {
+            //show help
+            MessageBox.Show("rip");
+        }
+
+        private void tsmiAboutGameLJ_Click(object sender, EventArgs e)
+        {
+            //show info about game
+            MessageBox.Show("This game is an student application for gaining experiance./n/nThe rights belong to ROC-terAa Helmond ICT-college");
+        }
+
+        private void tsmiDevLucLJ_Click(object sender, EventArgs e)
+        {
+            //show info about Luc Sieben
+            MessageBox.Show("Luc Sieben is one of the two student developers who made this application.\n\n2nd grade college at ROC-TerAa in Helmond in 2015-2016");
+        }
+
+        private void tsmiDevJenneLJ_Click(object sender, EventArgs e)
+        {
+            //show info about Jenne van den Boom
+            MessageBox.Show("Jenne van den Boom is one of the two student developers who made this application.\n\n2nd grade college at ROC-TerAa in Helmond in 2015-2016");
         }
     }
 }
